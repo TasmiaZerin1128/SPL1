b4504873596b9f8cb8e43ed00adbfe4d5eb3839f
Tasmia Zerin <60105612+TasmiaZerin1128@users.noreply.github.com>
2020-03-11 10:41:43 +0600

    Add files via upload
    This is the latest version of SPL1 project, since 12 march.

---
+++ firstPage/src/module-info.java
@@ -0,0 +1,9 @@
+module firstPage {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires jdk.javadoc;
+
+    opens First;
+}
